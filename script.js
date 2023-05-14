c58c6e3f2918231ae845d6a9348d6f5ec408883b
Giorgi  Beriashvili <129592253+giorgi-beriashvili01@users.noreply.github.com>
2023-05-14 09:23:08 +0400

    homework is done

---
+++ script.js
@@ -0,0 +1,13 @@
+let Name = "giorgi";
+let lastName = "beriashvili";
+let age = "22";
+const gender = "male";
+
+console.log(Name);
+console.log(lastName);
+console.log(age);
+console.log(gender);
+
+let rem = 101 % 2;
+
+console.log(rem);
